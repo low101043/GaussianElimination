cbd45a59ab2951c6c1f2a6874f2d8c5d924495bb
Nathaniel Lowis <nathaniel@lowis.co.uk>
2020-06-17 16:44:03 +0100

    Intial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Guassian Elimination
+
+This is a program which performs Guassian Elimination.  It was started in December 2019.  Needs to be tested and checked.  I do not believe that the actual Guassian Elimination works.
---
+++ src/main/java/com/natlowis/gauss/Matrix.java
@@ -0,0 +1,93 @@
+package com.natlowis.gauss;
+
+public class Matrix {
+	private int[][] matrix;
+	private int row;
+	private int col;
+
+	public Matrix(int row, int col) {
+
+		matrix = new int[row][col];
+		this.row = row;
+		this.col = col;
+
+		// System.out.println(row);
+		// System.out.println(col);
+
+		// this.print();
+
+	}
+	
+	
+	public Matrix(int[][] data) {
+		this.matrix = data;
+		this.row = data.length;
+		this.col = data[0].length;
+		
+	}
+	public void print() {
+
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[0].length; j++) {
+
+				System.out.print(matrix[i][j] + ", ");
+
+			}
+			System.out.println("");
+		}
+	}
+
+	public void add(int row, int col, int data) {
+
+		matrix[row][col] = data;
+	}
+
+	public void add(int row, int[] rowData) {
+
+		matrix[row] = rowData;
+	}
+
+	public int[][] matrix() {
+
+		return matrix;
+	}
+
+	public int size() {
+
+		return row * col;
+	}
+
+	public int[] row(int rowNumber) {
+
+		int[] rowToBeReturned = matrix[rowNumber];
+		// System.out.println(a);
+		return rowToBeReturned;
+
+	}
+
+	public int[] col(int colNumber) {
+
+		int[] colToBeReturned = new int[row];
+
+		for (int i = 0; i < row; i++) {
+			colToBeReturned[i] = matrix[i][colNumber];
+		}
+
+		return colToBeReturned;
+	}
+
+	public int columns() {
+		return col;
+	}
+
+	public int rows() {
+
+		return row;
+	}
+	
+	public int data(int rowData, int colData) {
+		
+		return matrix[rowData][colData];
+	}
+
+}
